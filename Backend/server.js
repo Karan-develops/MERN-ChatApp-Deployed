fa77961b6f0d3e40ea3deb9afa7dfb20afca423f
Karan-develops <mrkaran2k5@gmail.com>
2024-10-10 19:45:59 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messagesRoutes from "./routes/message.routes.js";
@@ -11,6 +12,9 @@ import { app, server } from "./socket/socket.js";
 
 const port = process.env.PORT || 5000;
 
+// This is for deployment
+const __dirname = path.resolve();
+
 dotenv.config();
 
 // middlewares for routes
@@ -20,6 +24,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messagesRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
 
 server.listen(port, () => {
   connectToMongoDB();
